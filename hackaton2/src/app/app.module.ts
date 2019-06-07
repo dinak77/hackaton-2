76aed4480f78941452444f13d138366fc03d2fe5
TEGNY Thomas <thomas.tegny06@gmail.com>
2019-06-07 11:10:41 +0200

    create a page comparator

--- hackaton2/src/app/app.module.ts
+++ hackaton2/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ComparatorComponent } from './comparator/comparator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComparatorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
